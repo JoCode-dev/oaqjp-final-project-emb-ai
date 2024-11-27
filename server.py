0ceab7691b7ebfbdf8c59d7fc9632b83c5bfc5e7
JoCode-dev <46027270+JoCode-dev@users.noreply.github.com>
2024-11-27 01:31:19 +0000

    fix: String

--- server.py
+++ server.py
@@ -8,8 +8,7 @@ def emotionDetector():
     text = request.args.get('textToAnalyze')
     emotions = emotion_detector(text)
 
-    return f"For the given statement, the system response is 'anger': {emotions["anger"]}, 'disgust': {emotions["disgust"]}, 
-    'fear': {emotions["fear"]}, 'joy': {emotions["joy"]} and 'sadness': {emotions["sadness"]}. The dominant emotion is {emotions["dominant_emotion"]}. "
+    return f"For the given statement, the system response is 'anger': {emotions["anger"]}, 'disgust': {emotions["disgust"]}, 'fear': {emotions["fear"]}, 'joy': {emotions["joy"]} and 'sadness': {emotions["sadness"]}. The dominant emotion is {emotions["dominant_emotion"]}. "
 
 @app.route("/")
 def render_index():
